415be384c0bd26e061e5b3c3ab3a4947fd0bed97
helen <yun.helen.tao@gmail.com>
2022-10-31 11:00:57 +1000

    topics upload image

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -59,7 +59,7 @@
 
 @section('styles')
   <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
-@stop
+@endsection
 
 @section('scripts')
   <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
@@ -71,7 +71,17 @@
     $(document).ready(function() {
       var editor = new Simditor({
         textarea: $('#editor'),
+        upload: {
+          url: '{{ route('topics.upload_image') }}',
+          params: {
+            _token: '{{ csrf_token() }}'
+          },
+          fileKey: 'upload_file',
+          connectionCount: 3,
+          leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+        },
+        pasteImage: true,
       });
     });
   </script>
-@stop
+@endsection
