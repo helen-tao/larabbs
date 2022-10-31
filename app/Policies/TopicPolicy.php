73a3efc58caac7b15f55fee99d187a5557978d78
helen <yun.helen.tao@gmail.com>
2022-10-31 22:20:04 +1000

    delete topic

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -12,4 +12,9 @@ class TopicPolicy extends Policy
     {
         return $topic->user_id == $user->id;
     }
+
+    public function destroy (User $user, Topic $topic)
+    {
+        return $topic->user_id == $user->id;
+    }
 }
