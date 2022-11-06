756a9088dc2b024363b60b28e71958b37a02032f
helen <yun.helen.tao@gmail.com>
2022-11-06 10:32:05 +1000

    activity users list

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -10,6 +10,8 @@ use App\Models\Category;
 use Illuminate\Support\Facades\Auth;
 use App\Handlers\ImageUploadHandler;
 
+use App\Models\User;
+
 class TopicsController extends Controller
 {
     public function __construct()
@@ -17,12 +19,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show','edit']]);
     }
 
-    public function index(Request $request, Topic $topic)
+    public function index(Request $request, Topic $topic, User $user)
     {
         $topics = $topic->withOrder($request->order)
                         ->with('user', 'category')  // 预加载防止 N+1 问题
                         ->paginate(30);
-        return view('topics.index', compact('topics'));
+        $active_users = $user->getActiveUsers();
+        //dd($active_users);
+        return view('topics.index', compact('topics','active_users'));
     }
 
     public function show(Request $request,Topic $topic)
--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable implements MustVerifyEmail
     }
 
     use HasRoles;
+    use Traits\ActiveUserHelper;
 
     protected $fillable = [
         'name',
