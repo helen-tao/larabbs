c90b4ed816e5efee300d428f62871382f181d3ca
helen <yun.helen.tao@gmail.com>
2022-11-06 19:31:43 +1000

    last activity

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     use HasRoles;
     use Traits\ActiveUserHelper;
+    use Traits\LastActivedAtHelper;
 
     protected $fillable = [
         'name',
